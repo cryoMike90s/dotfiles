2f10d8093a25b8d81860d1e8ea4af4839276e4bb
miskers <miskers@protonmail.com>
2025-06-04 10:50:49 +0000

    Changes to manual ubuntu bootstrap // add lazygit config

--- scripts/ubuntu_bootstrap.sh
+++ scripts/ubuntu_bootstrap.sh
@@ -2,10 +2,19 @@
 
 set -eu -o pipefail # fail on error and report it, debug all lines
 
-export INITIAL_PACKAGES="neovim jq make rsync stow tmux"
+export INITIAL_PACKAGES="stow ninja-build gettext cmake curl build-essential"
 
 echo $INITIAL_PACKAGES
 
 apt-get update
 apt-get upgrade
 apt-get install -y $INITIAL_PACKAGES
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+export LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
